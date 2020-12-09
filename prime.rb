7a93919b89784486e38d7c5157667e0c3ab057e7
chrislemus <icrislemus@gmail.com>
2020-12-09 15:29:36 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(int)
+  i = 2
+  int = int.abs
+  return false if int.between?(0,1) 
+  while i < int do
+    return false if int % i == 0
+    i += 1
+  end
+  return true
+end
